c640400f5ef5125e05d538cf67de13471ae438ba
Obaid Kazi <31682298+ObaidKazi@users.noreply.github.com>
2022-06-02 13:20:31 +0530

    Update cars.js

--- backend/router/cars.js
+++ backend/router/cars.js
@@ -25,6 +25,25 @@ router.get('/getAllCars', async function (req, res) {
         return res.status(500).send({ 'error': 'something went wrong' });
     }
 });
+router.get('/getCurrentUserCar/:user_id',async function (req, res) {
+    try {
+        const cars=await Car.findAll({where:{user_id:req.params.user_id}});
+         if(cars){
+            return res.status(200).send({ 'success': cars });
+        }else{
+            return res.status(404).send({ '204': 'data not found' });
+        }  
+    } catch (e) {
+        if (!fs.existsSync(directoryPath)) {
+            fs.mkdirSync(directoryPath, 0744);
+        }
+        let date_ob = new Date();
+        fs.appendFile(directoryPath + '/cars.log', `\n\n --- \t\t ${date_ob} --- \n\n` + e.toString(), (err) => {
+            if (err) console.error(err)
+        });
+        return res.status(500).send({ 'error': 'something went wrong' });
+    }
+});
 
 
 module.exports = router;
